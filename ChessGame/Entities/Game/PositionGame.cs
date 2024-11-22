5022ce3ab6dde60bc8b56b87a970b9f4f5500416
Fernando Monteiro <nandobenevides2520@gmail.com>
2024-11-22 07:27:57 -0300

    creation and basic structures of PositionGame class

---
+++ ChessGame/Entities/Game/PositionGame.cs
@@ -0,0 +1,28 @@
+﻿using board;
+
+namespace Game
+{
+    internal class PositionGame
+    {
+        public char Pilar { get; set; }
+        public int Position { get; set; }
+
+        public PositionGame(char pilar, int position)
+        {
+            this.Pilar = pilar;
+            this.Position = position;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Position, Pilar - 'a');
+        }
+
+        public override string ToString()
+        {
+
+            return "" + Pilar + Position;
+        }
+    }
+}
+
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -3,18 +3,10 @@ using board;
 using ChessGame;
 using Game;
 
-try
-{
-    Board board = new Board(8, 8);
-    board.PutPiece(new Tower(board, Color.Black), new Position(0, 0));
-    board.PutPiece(new Tower(board, Color.Black), new Position(1, 3));
-    board.PutPiece(new King(board, Color.Black), new Position(2, 4));
-    Screen.PrintBoard(board);
-}
-
-catch (BoardExeption exeption)
-{
-    Console.WriteLine(exeption.Message);
-}
+PositionGame pos = new PositionGame('c', 7);
+Console.WriteLine(pos);
+Console.WriteLine(pos.ToPosition());
 Console.ReadLine();
+
+
 
