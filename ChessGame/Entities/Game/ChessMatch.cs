b75ed9f2125fdacd1c2bbd291d68aa87d765c296
Fernando Monteiro <nandobenevides2520@gmail.com>
2024-11-24 02:51:39 -0300

    added en passant move and fixed bugs

--- ChessGame/Entities/Game/ChessMatch.cs
+++ ChessGame/Entities/Game/ChessMatch.cs
@@ -16,6 +16,7 @@ namespace Game
         public bool Finished { get; private set; }
 
         public bool Check { get; private set; }
+        public Piece VulnerableEnPassant;
 
         private HashSet<Piece> pices;
         private HashSet<Piece> captured;
@@ -27,6 +28,7 @@ namespace Game
             CurrentPlayer = Color.White;
             Finished = false;
             Check = false;
+            VulnerableEnPassant = null;
             pices = new HashSet<Piece>();
             captured = new HashSet<Piece>();
             PutPieces();
@@ -67,6 +69,25 @@ namespace Game
                 board.PutPiece(T, destinyT);
             }
 
+            //en passant
+            if (p is Pawn)
+            {
+                if (origin.Column != destination.Column && capturedPiece == null)
+                {
+                    Position posP;
+                    if(p.Color == Color.White)
+                    {
+                        posP = new Position(destination.Line + 1, destination.Column);
+                    }
+                    else
+                    {
+                        posP = new Position(destination.Line - 1, destination.Column);
+                    }
+                    capturedPiece = board.RemovePiece(posP);
+                    captured.Add(capturedPiece);
+                }
+            }
+
             return capturedPiece;
         }
 
@@ -99,6 +120,26 @@ namespace Game
                 T.MinusMoveAmount();
                 board.PutPiece(T, originT);
             }
+
+            // special move en passant
+            if (p is Pawn)
+            {
+                if(origin.Column != destination.Column && capturedPiece == VulnerableEnPassant)
+                {
+                    Piece pawn = board.RemovePiece(destination);
+                    Position posP;
+                    if (p.Color == Color.White)
+                    {
+                        posP = new Position(3, destination.Column);
+                    }
+                    else
+                    {
+                        posP= new Position(4, destination.Column);
+                    }
+                    board.PutPiece(pawn, posP);
+                }
+            }
+
         }
         public void RealizePlay(Position origin, Position destination)
         {
@@ -117,207 +158,223 @@ namespace Game
                 Check = false;
             }
 
-            if(CheckTest(Enemy(CurrentPlayer)))
+            if (CheckTest(Enemy(CurrentPlayer)))
             {
                 Finished = true;
             }
-
-
-            Turn++;
-            PlayerChange();
-        }
-
-        public void OriginPValidation(Position pos)
-        {
-            if (board.piece(pos) == null)
+            else
             {
-                throw new BoardExeption("The Origin Position is empty, select a piece!");
-            }
-            if (CurrentPlayer != board.piece(pos).Color)
-            {
-                throw new BoardExeption("This piece is not from your team!!");
-            }
-            if (!board.piece(pos).ThereIsPossibleMovement())
-            {
-                throw new BoardExeption("There are no possible movements for this piece!");
+                Turn++;
+                PlayerChange();
             }
 
-        }
-        public void DestinyPValidation(Position origin, Position destination)
-        {
-            if (!board.piece(origin).PossibleMove(destination))
+            Piece p = board.piece(destination);
+
+            //#special move en passant
+            if (p is Pawn && (destination.Line == origin.Line - 2 || destination.Line == origin.Line + 2))
             {
-                throw new BoardExeption("Invalid Destitation");
-            }
-        }
-        private void PlayerChange()
-        {
-            if (CurrentPlayer == Color.White)
-            {
-                CurrentPlayer = Color.Black;
+                VulnerableEnPassant = p;
+
             }
             else
             {
-                CurrentPlayer = Color.White;
+                VulnerableEnPassant = null;
             }
         }
 
-
-        public HashSet<Piece> CapturedPieces(Color color)
-        {
-            HashSet<Piece> aux = new HashSet<Piece>();
-            foreach (Piece x in captured)
+            public void OriginPValidation(Position pos)
             {
-                if (x.Color == color)
+                if (board.piece(pos) == null)
                 {
-                    aux.Add(x);
+                    throw new BoardExeption("The Origin Position is empty, select a piece!");
+                }
+                if (CurrentPlayer != board.piece(pos).Color)
+                {
+                    throw new BoardExeption("This piece is not from your team!!");
+                }
+                if (!board.piece(pos).ThereIsPossibleMovement())
+                {
+                    throw new BoardExeption("There are no possible movements for this piece!");
+                }
+
+            }
+            public void DestinyPValidation(Position origin, Position destination)
+            {
+                if (!board.piece(origin).PossibleMove(destination))
+                {
+                    throw new BoardExeption("Invalid Destitation");
                 }
             }
-            return aux;
-        }
-
-        public HashSet<Piece> pieceInGame(Color color)
-        {
-            HashSet<Piece> aux = new HashSet<Piece>();
-            foreach (Piece x in pices)
+            private void PlayerChange()
             {
-                if (x.Color == color)
+                if (CurrentPlayer == Color.White)
                 {
-                    aux.Add(x);
+                    CurrentPlayer = Color.Black;
+                }
+                else
+                {
+                    CurrentPlayer = Color.White;
                 }
             }
-            aux.ExceptWith(CapturedPieces(color));
-            return aux;
-        }
 
-        private Color Enemy(Color color)
-        {
-            if (color == Color.White)
-            {
-                return Color.Black;
-            }
-            else
-            {
-                return Color.White;
-            }
-        }
 
-        private Piece King(Color color)
-        {
-            foreach (Piece x in pieceInGame(color))
+            public HashSet<Piece> CapturedPieces(Color color)
             {
-                if (x is King)
+                HashSet<Piece> aux = new HashSet<Piece>();
+                foreach (Piece x in captured)
                 {
-                    return x;
-                }
-            }
-            return null;
-        }
-
-        public bool IsInCheck(Color color)
-        {
-            Piece R = King(color);
-            if (R == null)
-            {
-                throw new BoardExeption("Theres no king with the colour " + color + "in the board");
-            }
-
-            foreach (Piece x in pieceInGame(Enemy(color)))
-            {
-                bool[,] mat = x.PossibleMoves();
-                if (mat[R.Position.Line, R.Position.Column])
-                {
-                    return true;
-                }
-            }
-            return false;
-
-        }
-
-        public bool CheckTest(Color color)
-        {
-            if (!IsInCheck(color))
-            {
-                return false;
-            }
-            foreach(Piece x in pieceInGame(color))
-            {
-                bool[,] mat = x.PossibleMoves();
-                for(int i = 0; i<board.Lines;  i++)
-                {
-                    for(int j = 0; j<board.Columns; j++)
+                    if (x.Color == color)
                     {
-                        if (mat[i, j])
+                        aux.Add(x);
+                    }
+                }
+                return aux;
+            }
+
+            public HashSet<Piece> pieceInGame(Color color)
+            {
+                HashSet<Piece> aux = new HashSet<Piece>();
+                foreach (Piece x in pices)
+                {
+                    if (x.Color == color)
+                    {
+                        aux.Add(x);
+                    }
+                }
+                aux.ExceptWith(CapturedPieces(color));
+                return aux;
+            }
+
+            private Color Enemy(Color color)
+            {
+                if (color == Color.White)
+                {
+                    return Color.Black;
+                }
+                else
+                {
+                    return Color.White;
+                }
+            }
+
+            private Piece King(Color color)
+            {
+                foreach (Piece x in pieceInGame(color))
+                {
+                    if (x is King)
+                    {
+                        return x;
+                    }
+                }
+                return null;
+            }
+
+            public bool IsInCheck(Color color)
+            {
+                Piece R = King(color);
+                if (R == null)
+                {
+                    throw new BoardExeption("Theres no king with the colour " + color + "in the board");
+                }
+
+                foreach (Piece x in pieceInGame(Enemy(color)))
+                {
+                    bool[,] mat = x.PossibleMoves();
+                    if (mat[R.Position.Line, R.Position.Column])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            }
+
+            public bool CheckTest(Color color)
+            {
+                if (!IsInCheck(color))
+                {
+                    return false;
+                }
+                foreach (Piece x in pieceInGame(color))
+                {
+                    bool[,] mat = x.PossibleMoves();
+                    for (int i = 0; i < board.Lines; i++)
+                    {
+                        for (int j = 0; j < board.Columns; j++)
                         {
-                            Position origin = x.Position;
-                            Position destiny = new Position(i, j);
-                            Piece capturedPiece = ExecuteMovement(origin, destiny);
-                            bool checktest = IsInCheck(color);
-                            UndoMove(origin, destiny,capturedPiece);
-                            if (!checktest)
+                            if (mat[i, j])
                             {
-                                return false;
+                                Position origin = x.Position;
+                                Position destiny = new Position(i, j);
+                                Piece capturedPiece = ExecuteMovement(origin, destiny);
+                                bool checktest = IsInCheck(color);
+                                UndoMove(origin, destiny, capturedPiece);
+                                if (!checktest)
+                                {
+                                    return false;
+                                }
                             }
                         }
                     }
                 }
+                return true;
             }
-            return true;
-        }
 
-        public void AddNewPiece(char column, int line, Piece piece)
-        {
-            board.PutPiece(piece, new PositionGame(column, line).ToPosition());
-            pices.Add(piece);
-        }
+            public void AddNewPiece(char column, int line, Piece piece)
+            {
+                board.PutPiece(piece, new PositionGame(column, line).ToPosition());
+                pices.Add(piece);
+            }
 
-        private void PutPieces()
-        {
-            AddNewPiece('a', 1, new Tower(board, Color.White));
-            AddNewPiece('b', 1, new Horse(board, Color.White));
-            AddNewPiece('c', 1, new Bishop(board, Color.White));
-            AddNewPiece('d', 1, new Queen(board, Color.White));
-            AddNewPiece('e', 1, new King(board, Color.White, this));
-            AddNewPiece('f', 1, new Bishop(board, Color.White));
-            AddNewPiece('g', 1, new Horse(board, Color.White));
-            AddNewPiece('h', 1, new Tower(board, Color.White));
-            AddNewPiece('a', 2, new Pawn(board, Color.White));
-            AddNewPiece('b', 2, new Pawn(board, Color.White));
-            AddNewPiece('c', 2, new Pawn(board, Color.White));
-            AddNewPiece('d', 2, new Pawn(board, Color.White));
-            AddNewPiece('e', 2, new Pawn(board, Color.White));
-            AddNewPiece('f', 2, new Pawn(board, Color.White));
-            AddNewPiece('g', 2, new Pawn(board, Color.White));
-            AddNewPiece('h', 2, new Pawn(board, Color.White));
+            private void PutPieces()
+            {
+                AddNewPiece('a', 1, new Tower(board, Color.White));
+                AddNewPiece('b', 1, new Horse(board, Color.White));
+                AddNewPiece('c', 1, new Bishop(board, Color.White));
+                AddNewPiece('d', 1, new Queen(board, Color.White));
+                AddNewPiece('e', 1, new King(board, Color.White, this));
+                AddNewPiece('f', 1, new Bishop(board, Color.White));
+                AddNewPiece('g', 1, new Horse(board, Color.White));
+                AddNewPiece('h', 1, new Tower(board, Color.White));
+                AddNewPiece('a', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('b', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('c', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('d', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('e', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('f', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('g', 2, new Pawn(board, Color.White, this));
+                AddNewPiece('h', 2, new Pawn(board, Color.White, this));
 
 
-            
 
-            AddNewPiece('a', 8, new Tower(board, Color.Black));
-            AddNewPiece('b', 8, new Horse(board, Color.Black));
-            AddNewPiece('c', 8, new Bishop(board, Color.Black));
-            AddNewPiece('d', 8, new Queen(board, Color.Black));
-            AddNewPiece('e', 8, new King(board, Color.Black, this));
-            AddNewPiece('f', 8, new Bishop(board, Color.Black));
-            AddNewPiece('g', 8, new Horse(board, Color.Black));
-            AddNewPiece('h', 8, new Tower(board, Color.Black));
-            AddNewPiece('a', 7, new Pawn(board, Color.Black));
-            AddNewPiece('b', 7, new Pawn(board, Color.Black));
-            AddNewPiece('c', 7, new Pawn(board, Color.Black));
-            AddNewPiece('d', 7, new Pawn(board, Color.Black));
-            AddNewPiece('e', 7, new Pawn(board, Color.Black));
-            AddNewPiece('f', 7, new Pawn(board, Color.Black));
-            AddNewPiece('g', 7, new Pawn(board, Color.Black));
-            AddNewPiece('h', 7, new Pawn(board, Color.Black));
+
+                AddNewPiece('a', 8, new Tower(board, Color.Black));
+                AddNewPiece('b', 8, new Horse(board, Color.Black));
+                AddNewPiece('c', 8, new Bishop(board, Color.Black));
+                AddNewPiece('d', 8, new Queen(board, Color.Black));
+                AddNewPiece('e', 8, new King(board, Color.Black, this));
+                AddNewPiece('f', 8, new Bishop(board, Color.Black));
+                AddNewPiece('g', 8, new Horse(board, Color.Black));
+                AddNewPiece('h', 8, new Tower(board, Color.Black));
+                AddNewPiece('a', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('b', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('c', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('d', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('e', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('f', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('g', 7, new Pawn(board, Color.Black, this));
+                AddNewPiece('h', 7, new Pawn(board, Color.Black, this));
 
 
 
 
 
-        }
+            }
 
 
 
 
+        
     }
 }
+
--- ChessGame/Entities/Game/Pawn.cs
+++ ChessGame/Entities/Game/Pawn.cs
@@ -10,7 +10,10 @@ namespace Game
 {
     class Pawn : Piece
     {
-        public Pawn(Board boad, Color color) : base(boad, color) { }
+        private ChessMatch match;
+        public Pawn(Board boad, Color color, ChessMatch match) : base(boad, color) { 
+         this.match = match;
+        }
 
         public override string ToString()
         {
@@ -56,6 +59,21 @@ namespace Game
                 {
                     mat[pos.Line, pos.Column] = true;
                 }
+                //#specialmove enpassant
+                if(Position.Line == 3)
+                {
+                    Position left = new Position(Position.Line, Position.Column - 1);
+                    if (Board.ValidPosition(left) && TheresEnemy(left) && Board.piece(left) == match.VulnerableEnPassant)
+                    {
+                        mat[left.Line - 1,left.Column] = true;
+                    }
+
+                    Position right = new Position(Position.Line, Position.Column + 1);
+                    if (Board.ValidPosition(right) && TheresEnemy(right) && Board.piece(right) == match.VulnerableEnPassant)
+                    {
+                        mat[right.Line - 1, right.Column] = true;
+                    }
+                }
 
             }
             else
@@ -82,6 +100,22 @@ namespace Game
                 if (Board.ValidPosition(pos) && TheresEnemy(pos)) 
                 {
                     mat[pos.Line, pos.Column] = true;
+                }
+
+                //#specialmove enpassant
+                if (Position.Line == 4)
+                {
+                    Position left = new Position(Position.Line, Position.Column - 1);
+                    if (Board.ValidPosition(left) && TheresEnemy(left) && Board.piece(left) == match.VulnerableEnPassant)
+                    {
+                        mat[left.Line + 1, left.Column] = true;
+                    }
+
+                    Position right = new Position(Position.Line, Position.Column + 1);
+                    if (Board.ValidPosition(right) && TheresEnemy(right) && Board.piece(right) == match.VulnerableEnPassant)
+                    {
+                        mat[right.Line + 1, right.Column] = true;
+                    }
                 }
 
             }
